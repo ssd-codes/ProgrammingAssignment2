c5940fa75ae229fbe5d467cfb50d43f988ae53fb
ssd-codes <165946741+ssd-codes@users.noreply.github.com>
2025-01-14 22:50:52 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To calculate inverse of a matrix
+## makeCacheMatrix creates a new matrix with input data
+## it has the set and get functions to initialize original matrix 
+## and also the inverse
+## Rather than calculating the inverse each time, this function returns
+## the cached inverse if the original matrix has not changed
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a new matrix with input data
+## it has the set and get functions to initialize original matrix 
+## and also the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL 
+  
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x 
+  
+  setInverse <- function(inv) inverse <<- inv
+  
+  getInverse <- function() inverse
+  
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## First time it computes the inverse and puts it in the cache
+## Rather than calculating the inverse each time, this function returns
+## the cached inverse if the original matrix has not changed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("Returning inverse from cache")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setInverse(inverse)
+  inverse
 }
